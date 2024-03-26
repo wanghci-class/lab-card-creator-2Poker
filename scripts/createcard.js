16dcee69d8def54be0efba98aeca27b69d768c7f
2Poker <randpgamz@gmail.com>
2024-03-26 15:29:08 -0400

    finished

--- scripts/createcard.js
+++ scripts/createcard.js
@@ -1,18 +1,36 @@
 
 let preview = document.getElementById('preview');
-
+let save = document.getElementById('save');
 
 preview.addEventListener('click', () => {
-    let to = document.getElementById('to').value;
-    let from = document.getElementById('from').value;
-    let title = document.getElementById('title').value;
-    let subtitle = document.getElementById('subtitle').value;
-    let message = document.getElementById('message').value;
 
-    document.getElementsByClassName('to-text')[0].textContent = to;
-    document.getElementsByClassName('from-text')[0].textContent = from;
-    document.getElementsByClassName('title-text')[0].textContent = title;
-    document.getElementsByClassName('subtitle-text')[0].textContent = subtitle;
-    document.getElementsByClassName('message-text')[0].textContent = message;
+    document.getElementsByClassName('to-text')[0].textContent = document.getElementById('to').value;
+    document.getElementsByClassName('from-text')[0].textContent = document.getElementById('from').value;
+    document.getElementsByClassName('title-text')[0].textContent = document.getElementById('title').value;
+    document.getElementsByClassName('subtitle-text')[0].textContent = document.getElementById('subtitle').value;
+    document.getElementsByClassName('message-text')[0].textContent = document.getElementById('message').value;
 
+});
+
+save.addEventListener('click', (ev) => {
+    ev.preventDefault();
+
+    let cards = localStorage.getItem('cards');
+    if (cards == null) {
+        cards = [];
+    } else {
+        cards = JSON.parse(cards);
+    }
+
+    let card = {
+        to: document.getElementById('to').value,
+        from: document.getElementById('from').value,
+        title: document.getElementById('title').value,
+        subtitle: document.getElementById('subtitle').value,
+        message: document.getElementById('message').value,
+    };
+
+    cards.push(card);
+
+    localStorage.setItem('cards', JSON.stringify(cards));
 });
